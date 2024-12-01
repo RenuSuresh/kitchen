3ff088c51bad4af8a5dc4fb5d5f9ea6bf7979a3e
RenuPE101 <renu.ganvir@pharmeasy.in>
2024-12-01 17:55:44 +0530

    api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,17 +1,17 @@
-import type { Request, Response, Application } from "express";
-import express from "express";
+import express, { Request, Response, Application } from "express";
 
 const app: Application = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 4009;
 
+// @ts-ignore
 app.get("/", (_req: Request, res: Response) => {
 	return res.send("Express Typescript on Vercel");
 });
-
+// @ts-ignore
 app.get("/ping", (_req: Request, res: Response) => {
 	return res.send("pong 🏓");
 });
 
 app.listen(port, () => {
-	console.log(`Server is listening on ${port}`);
+	return console.log(`Server is listening on ${port}`);
 });
